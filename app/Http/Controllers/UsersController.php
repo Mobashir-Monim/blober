7f7912caeb39e3e2877d29808445f86409bd06cd
Mobashir Monim <mobashirmonim@gmail.com>
2020-04-22 22:50:00 +0600

    Write test for root

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -46,7 +46,7 @@ class UsersController extends Controller
 
     public function create()
     {
-        dd(request()->getHttpHost());
+        dd(request()->root());
         return view('users.create');
     }
 
