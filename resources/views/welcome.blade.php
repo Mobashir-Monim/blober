e9b388cc28ab9eb9731df42c92e7f2ca8116cdf2
Mobashir Monim <mobashirmonim@gmail.com>
2020-04-25 23:32:16 +0600

    Publish error pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -97,17 +97,6 @@
                 </div>
 
                 <div class="lg-font bg-dark text-white card-rounded px-5">Proficiency in MySQL Queries</div>
-
-                {{-- <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div> --}}
             </div>
         </div>
         <footer class="footer bg-dark text-white">
