e228e30ff7294679535eb0dcd2f3c6af0996dc88
Mobashir Monim <mobashirmonim@gmail.com>
2020-04-29 14:28:54 +0600

    Update image

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}">
-<img src="{{ request()->root() }}/img/blober-w-t.svg" alt="{{ $slot }}">
+<img src="{{ asset('img/blober-w-t.svg') }}" alt="{{ $slot }}">
 </a>
 </td>
 </tr>
