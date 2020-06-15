d07a3a13cdbe909fedc119047c7eaab660dc6656
Mobashir Monim <mobashirmonim@gmail.com>
2020-06-15 14:46:55 +0600

    Update seed

--- app/Http/Middleware/HasQuizMiddleware.php
+++ app/Http/Middleware/HasQuizMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Carbon\Carbon;
 use App\Quiz;
+use App\Section;
 
 class HasQuizMiddleware
 {
@@ -20,7 +21,7 @@ class HasQuizMiddleware
         $now = Carbon::now()->toDateTimeString();
         
         if (auth()->user()->student != null) {
-            if (!is_null(Quiz::where('section', auth()->user()->student->section)->where('start', '<=', $now)->where('end', '>', $now)->first())) {
+            if (!is_null(Quiz::where('section', Section::find(auth()->user()->student->section)->section)->where('start', '<=', $now)->where('end', '>', $now)->first())) {
                 return $next($request);
             } else {
                 return back()->with('error', 'There are no valid quizzes available for you right now');
